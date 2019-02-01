3fc3875e9d1e5ce11fd359322be573e755dc168d
nbech <nbech10@gmail.com>
2019-02-01 15:46:10 +0100

    ProductList done, Product and Details inprogress + new Title and context component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
     render() {
+        const {id, title, img, price, inCart} = this.props.product;
         return (
-            <div>
-                <h3>Hello from Product</h3>
+            <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={console.log('You clicked me on the image container')}>
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top"></img>
+                        </Link>
+                        <button className="cart-btn" 
+                            disabled={inCart?true : false}
+                            onClick={()=>{console.log('added to the cart');}}
+                        >
+                            {inCart ? (
+                               <p className="text-capitalize mb-0" disabled>
+                                   {" "}
+                                  in cart
+                                </p>
+                            ) : (
+                                <i className="fas fa-cart-plus"></i>
+                            )}
+                        </button>
+                    </div>
+                    {/* card footer */}
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">$</span>
+                            {price}
+                        </h5>
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+Product.propTypes = {
+    product: PropTypes.shape({
+        id:PropTypes.number,
+        img: PropTypes.string,
+        title: PropTypes.string,
+        price: PropTypes.number,
+        inCart: PropTypes.bool
+    }).isRequired
+};
